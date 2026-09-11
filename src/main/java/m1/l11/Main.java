15658fb285541120556ebf6db9b32e3f2b4568bf
Valerii <fabolo1234@gmail.com>
2026-09-11 19:32:29 +0300

    init

--- src/main/java/m1/l11/Main.java
+++ src/main/java/m1/l11/Main.java
@@ -3,6 +3,7 @@ package m1.l11;
 public class Main {
     static void main() {
         System.out.printf("***Hello World!***%n");
+        System.out.println("Hello, World!");
     }
 
     private static void printHello() {
