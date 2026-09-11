fb47d3c83e5a03c6a5a287a975011094c65dd221
Valerii <fabolo1234@gmail.com>
2026-09-11 20:47:34 +0300

    add sum method

---
+++ src/main/java/m1/l11/Calc.java
@@ -0,0 +1,10 @@
+package m1.l11;
+
+public class Calc {
+    static void main() {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
