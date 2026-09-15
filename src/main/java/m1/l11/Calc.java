ad35e21602efd723882cd4e9643ba7addbd6d071
Valerii <fabolo1234@gmail.com>
2026-09-15 18:49:56 +0300

    add subtraction and multiplication methods

--- src/main/java/m1/l11/Calc.java
+++ src/main/java/m1/l11/Calc.java
@@ -6,5 +6,11 @@ public class Calc {
         int b = 10;
         int sum = a + b;
         System.out.println("Sum: " + sum);
+
+        int subtract = b - a;
+        System.out.println("Subtract: " + subtract);
+
+        int multiply = a * b;
+        System.out.println("Multiply: " + multiply);
     }
 }
